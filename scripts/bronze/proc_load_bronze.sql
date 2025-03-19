3095ff854d2e0c6efc4fddacab90e18799daff4b
Agata <dziobak.mobile2012@gmail.com>
2025-03-19 14:20:07 +0100

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -9,6 +9,8 @@
         - The procedure includes error handling to log any issues during the load process.
 *********************************************************************************/
 
+--EXEC bronze.load_bronze
+
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
     DECLARE @start_time DATETIME, @end_time DATETIME;
@@ -26,7 +28,7 @@ BEGIN
         SET @start_time = GETDATE();
         TRUNCATE TABLE bronze.crm_cust_info;
         BULK INSERT bronze.crm_cust_info
-        FROM 'j:\Desktop\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+        FROM 'C:\Users\Agata\Desktop\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
         WITH (
             FIRSTROW = 2, -- we have headers in the first row
             FIELDTERMINATOR = ',',
@@ -40,7 +42,7 @@ BEGIN
         SET @start_time = GETDATE();
         TRUNCATE TABLE bronze.crm_prd_info;
         BULK INSERT bronze.crm_prd_info
-        FROM 'j:\Desktop\sql-data-warehouse-project\datasets\source_crm\cust_prd.csv'
+        FROM 'C:\Users\Agata\Desktop\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
         WITH (
             FIRSTROW = 2, -- we have headers in the first row
             FIELDTERMINATOR = ',',
@@ -54,7 +56,7 @@ BEGIN
         SET @start_time = GETDATE();
         TRUNCATE TABLE bronze.crm_sales_details;
         BULK INSERT bronze.crm_sales_details
-        FROM 'j:\Desktop\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+        FROM 'C:\Users\Agata\Desktop\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
         WITH (
             FIRSTROW = 2, -- we have headers in the first row
             FIELDTERMINATOR = ',',
@@ -72,7 +74,7 @@ BEGIN
         SET @start_time = GETDATE();
         TRUNCATE TABLE bronze.erp_cust_az12;
         BULK INSERT bronze.erp_cust_az12
-        FROM 'j:\Desktop\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+        FROM 'C:\Users\Agata\Desktop\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
         WITH (
             FIRSTROW = 2, -- we have headers in the first row
             FIELDTERMINATOR = ',',
@@ -86,7 +88,7 @@ BEGIN
         SET @start_time = GETDATE();
         TRUNCATE TABLE bronze.erp_loc_a101;
         BULK INSERT bronze.erp_loc_a101
-        FROM 'j:\Desktop\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+        FROM 'C:\Users\Agata\Desktop\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
         WITH (
             FIRSTROW = 2, -- we have headers in the first row
             FIELDTERMINATOR = ',',
@@ -100,7 +102,7 @@ BEGIN
         SET @start_time = GETDATE();
         TRUNCATE TABLE bronze.erp_px_cat_g1v2;
         BULK INSERT bronze.erp_px_cat_g1v2
-        FROM 'j:\Desktop\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+        FROM 'C:\Users\Agata\Desktop\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
         WITH (
             FIRSTROW = 2, -- we have headers in the first row
             FIELDTERMINATOR = ',',
